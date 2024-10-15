9ea9ebc979510637bee92087cd8f44a38dc05155
ZsakaiBalint <balintzsakai@gmail.com>
2024-10-15 20:05:05 +0200

    proposed example

---
+++ test/light-driver.test.ts
@@ -0,0 +1,12 @@
+import test from "ava";
+import uc from "integration-api";
+
+
+
+test("Example test case", (t) => {
+
+    uc.init("src/light-driver.json");
+    let driverVersion = uc.getDriverVersion().version.driver;
+
+    return t.is(driverVersion, "1.0.0");
+});
