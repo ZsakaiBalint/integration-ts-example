1e8a20bd0eff65b82a4064a94643c08e953c18aa
ZsakaiBalint <balintzsakai@gmail.com>
2024-10-18 14:17:16 +0200

    added test for driver_url

--- test/light-driver.test.ts
+++ test/light-driver.test.ts
@@ -1,12 +1,13 @@
 import test from "ava";
 import uc from "uc-integration-api";
 
+//driver_url field, where we had issues before. Check that the field is not set. 
 
-
-test("Example test case", (t) => {
+test("Check if driver_url is not defined, so it can be overwritten", (t) => {
 
     uc.init("src/light-driver.json");
-    let driverVersion = uc.getDriverVersion().version.driver;
+   
+    const driver_url = uc.getDriverUrl();
 
-    return t.is(driverVersion, "1.0.0");
+    return t.is(typeof driver_url, "undefined");
 });
