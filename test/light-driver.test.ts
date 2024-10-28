2d7cd236aec99b502a299b8b80d187043f70bfd6
ZsakaiBalint <balintzsakai@gmail.com>
2024-10-28 11:49:05 +0100

    removed the test github action and added a dummy test

--- test/light-driver.test.ts
+++ test/light-driver.test.ts
@@ -1,10 +1,7 @@
+//Define your tests in this file
+
 import test from "ava";
-import uc from "uc-integration-api";
 
-test("Check if driver_url is not defined, so it can be overwritten", (t) => {
-  uc.init("src/light-driver.json");
-
-  const driver_url = uc.getDriverUrl();
-
-  t.is(driver_url, undefined);
+test("Dummy test", (t) => {
+  t.true(true);
 });
